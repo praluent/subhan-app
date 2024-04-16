cc59324588f8718b9d07f9654a5f26ab40ac8ae3
Ali Syed <148507779+praluent@users.noreply.github.com>
2024-04-16 20:59:47 +0100

    first commit (please look at my other projects i'm better than this)

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,80 @@
+'use client'
+
 import Image from "next/image";
+import React, { useState, useRef } from "react";
 
 export default function Home() {
+  const [showText, setShowText] = useState(true);
+  const [open, setOpen] = useState(false);
+  const dialogRef = useRef(null);
+
+
+
   return (
+    
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      <dialog id="my_modal_2" className="modal">
+        <div className="modal-box">
+          <p className="py-4">Wrong Answer. Try again 🙃</p>
         </div>
-      </div>
+        <form method="dialog" className="modal-backdrop">
+          <button>close</button>
+        </form>
+      </dialog>
+      
+      {showText && 
+      <div>
+        <h1 className="text-4xl font-bold">i'm really sorry <div className="animate-pulse">🙃</div></h1>
+        <h2 className="animate-pulse">🌹</h2>
+        <h2>do you forgive me?</h2>
+            <h2>ﺍﺏ ﮐﮯ ﮨﻢ ﺑﭽﮭﮍﮮ ﺗﻮ ﺷﺎﯾﺪ ﮐﺒﮭﯽ
+      ﺧﻮﺍﺑﻮﮞ ﻣﯿﮟ ﻣﻠﯿﮟ <br />
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      ﺟﺲ ﻃﺮﺡ ﺳﻮﮐﮭﮯ ﮨﻮﺋﮯ ﭘﮭﻮﻝ ﮐﺘﺎﺑﻮﮞ
+      ﻣﯿﮟ ﻣﻠﯿﮟ
+      <br />
+      ﮈﮬﻮﻧﮉ ﺍﺟﮍﮮ ﮨﻮﺋﮯ ﻟﻮﮔﻮﮞ ﻣﯿﮟ ﻭﻓﺎ
+      ﮐﮯ ﻣﻮﺗﯽ
+      <br />
+      ﯾﮧ ﺧﺰﺍﻧﮯ ﺗﺠﮭﮯ ﻣﻤﮑﻦ ﮨﮯ ﺧﺮﺍﺑﻮﮞ
+      ﻣﯿﮟ ﻣﻠﯿﮟ
+      <br />
+      ﻏﻢِ ﺩﻧﯿﺎ ﺑﮭﯽ ﻏﻢِ ﮮﺍﺭ ﻣﯿﮟ ﺷﺎﻣﻞ ﮐﺮ
+      ﻟﻮ
+      <br />
+      ﻧﺸّﮧ ﺑﮍﮬﺘﺎ ﮨﮯ ﺷﺮﺍﺑﯿﮟ ﺟﺐ ﺷﺮﺍﺑﻮﮞ
+      ﻣﯿﮟ ﻣﻠﯿﮟ
+      <br />
+      ﺗُﻮ ﺧﺪﺍ ﮨﮯ ﻧﮧ ﻣﺮﺍ ﻋﺸﻖ ﻓﺮﺷﺘﻮﮞ
+      ﺟﯿﺴﺎ!
+      <br />
+      ﺩﻭﻧﻮﮞ ﺍﻧﺴﺎﮞ ﮨﯿﮟ ﺗﻮ ﮐﯿﻮﮞ ﺍﺗﻨﮯ
+      ﺣﺠﺎﺑﻮﮞ ﻣﯿﮟ ﻣﻠﯿﮟ
+      <br />
+      ﺁﺝ ﮨﻢ ﺩﺍﺭ ﭘﮧ ﮨﻢ ﮐﮭﯿﻨﭽﮯ ﮔﺌﮯ ﺟﻦ
+      ﺑﺎﺗﻮﮞ ﭘﺮ
+      <br />
+      ﮐﯿﺎ ﻋﺠﺐ ﮐﻞ ﻭﮦ ﺯﻣﺎﻧﮯ ﮐﻮ ﻧﺼﺎﺑﻮﮞ
+      ﻣﯿﮟ ﻣﻠﯿﮟ
+      <br />
+      ﺍﺏ ﻧﮧ ﻭﮦ ﻣﯿﮟ ﻧﮧ ﻭﮦ ﺗﻮ ﮨﮯ ﻧﮧ ﻭﮦ
+      ﻣﺎﺿﯽ ﮨﮯ ﻓﺮﺍﺯ
+      <br />
+      ﺟﯿﺴﮯ ﺩﻭ ﺷﺨّﺺ ﺗﻤﻨﺎ ﮐﮯ ﺳﺮﺍﺑﻮﮞ
+      ﻣﯿﮟ ﻣﻠﯿﮟ
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+        </h2>
+        <div className="flex justify-center">
+          <button className="m-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={()=>setShowText(false)}>
+            Yes
+          </button>
+          <button className="m-2 bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded" onClick={()=> document.getElementById('my_modal_2').showModal()}>
+            No 😢
+          </button>
+        </div>
+      </div>}
+      {!showText &&
+        <h1> 🙃 </h1> }
     </main>
   );
 }
